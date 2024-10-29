8247792af2150430185e79be30cb221a81f05ff2
pera <nikodijevicp01@gmail.com>
2024-10-29 21:19:23 +0100

    starter structure with lein made

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to amvi-compiler
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/amvi_compiler/core_test.clj
@@ -0,0 +1,7 @@
+(ns amvi-compiler.core-test
+  (:require [clojure.test :refer :all]
+            [amvi-compiler.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
